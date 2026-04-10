fd4128f3c0906001ce8f7483e4fd1555f53c7d4b
Kudoro <kudoroesther.learn@gmail.com>
2026-04-10 15:53:52 +0100

    fastapi

--- fastapi_practice.py
+++ fastapi_practice.py
@@ -1,7 +1,23 @@
 # Practicing FastAPI
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Optional
 
 app = FastAPI()
 
-@app.get()
+users = {
+    1 : {
+        "name": "Simeon",
+        "age": 52,
+        "level": "level 9"
+    }
+}
+
+# Get endpoint: gets information
+@app.get("/") # This takes it to the home
+def index():
+    return {"Home": "Welcome to the first trial."}
+
+# Path parameter
+@app.get("/get_user/{user_id}")
+def get_user(user_id: int):
+    return users[user_id]
