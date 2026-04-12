f2ef72b7933e7ea66bde7fe2a27fa1e0bf3ccfa6
Kudoro <kudoroesther.learn@gmail.com>
2026-04-12 20:32:15 +0100

    fastapi

--- fastapi_practice.py
+++ fastapi_practice.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,7 +10,13 @@ users = {
     1 : {
         "name": "Simeon",
         "age": 52,
-        "level": "level 9"
+        "year": "level 9"
+    },
+
+    2 : {
+        "name": "Adebo",
+        "age": 50,
+        "year": "level 9"
     }
 }
 
@@ -21,6 +28,8 @@ def index():
 # Path parameter
 @app.get("/get_user/{user_id}")
 def get_user(user_id: int):
+    if user_id not in users:
+        return {"Error": "User not found"}
     return users[user_id]
 
 # Query parameter
@@ -55,4 +64,43 @@ def combined(user_id: int, name: Optional[str] = None):
             return {"Error": "Name does not match User ID"}
     
     return user
+
+
+# REQUEST BODY AND POST METHOD
+
+class User(BaseModel):
+    name: str
+    age: int
+    year: str
+
+# Post method, creates or adds new information
+# The class User makes up the request body
+@app.post("/create-user{user_id}")
+def create_user(user_id: int, user : User):
+    if user_id in users:
+        return {"Error": "User exists"}
+    # This saves the input from 'user' to the dictionary 'users' using the 'user_id'
+    users[user_id] = user
+    return users[user_id]
+
+
+# PUT Method, updates or changes an existing information
+class Update(BaseModel):
+    #making it optional so it doesn't update fields that weren't provided by the user
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
+@app.post("/update-user/{user_id}")
+def update_user(user_id: int, user: Update):
+    if user_id not in users:
+        return {"Error": "Student not found"}
+    # This ensures that it only updates the fields provided, and it doesn't overwrite the ones not provided
+    if user.name != None:
+        users[user_id]["name" ]= user.name
+    if user.age != None:
+        users[user_id]["age"] = user.age
+    if user.year != None:
+        users[user_id]["year"] = user.year
+    return users[user_id]
 
