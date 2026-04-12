333e8121068bba8cbac0ae40878c9772957b0fd9
Kudoro <kudoroesther.learn@gmail.com>
2026-04-12 20:03:41 +0100

    fastapi

--- fastapi_practice.py
+++ fastapi_practice.py
@@ -1,6 +1,7 @@
 # Practicing FastAPI
 
-from fastapi import FastAPI, Path, Optional
+from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -29,3 +30,29 @@ def using_name(name: str):
         if users[user]["name"] == name:
             return users
     return {"Data": "Not found"}
+
+# Combininig Query and Path
+@app.get("/combine-user/{user_id}") #path parameter
+# def combined_user(*, name: Optional[str] = None, user_id: int):
+#     for user_id in users:
+#         if users[user_id]['name'] == name:
+#             return users[user_id]
+        
+#         return users[user_id]
+#     if user_id not in users:
+#         return {"Error": "User not found"}
+    
+# Gets the user details using user id and optionally name
+def combined(user_id: int, name: Optional[str] = None):
+    if user_id not in users:
+        return {"Error": "User not found"}
+    
+    user = users[user_id]
+    if name != None:
+        if user["name"] == name:
+            return user
+        else:
+            return {"Error": "Name does not match User ID"}
+    
+    return user
+
